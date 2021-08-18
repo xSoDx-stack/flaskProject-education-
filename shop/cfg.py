f2a6867eea6b655c549b84916272b60bde9797c1
SoD <maxmus_91@mail.ru>
2021-08-18 09:38:07 +0300

    16

--- shop/cfg.py
+++ shop/cfg.py
@@ -1,8 +1,11 @@
-import os
+import re
 from os import getenv
 
 class Configuration:
     DEBUG = 'True'
     SECRET_KEY = getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    database_path = getenv('DATABASE_URL')
+    if database_path.startswith('postgres://'):
+        database_path = database_path.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = database_path
